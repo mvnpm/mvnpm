9b4326eda105ffd6bf4d955ecc7bf132fd8aff4c
Phillip Kruger <phillip.kruger@gmail.com>
2022-10-17 12:44:30 +1100

    release v1.0.0
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- src/main/java/org/mvnpm/importmap/model/Imports.java
+++ src/main/java/org/mvnpm/importmap/model/Imports.java
@@ -1,7 +1,9 @@
 package org.mvnpm.importmap.model;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import java.util.Map;
 
+@RegisterForReflection
 public record Imports(
         Map<String,String> imports) {
 }
