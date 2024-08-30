e9f432348b0822e43ffcbf3d935903217927e35a
Phillip Kruger <phillip.kruger@gmail.com>
2024-08-30 21:59:00 +1000

    Removed Linux script
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- src/main/linux/opt/mvnpm/updateVersion.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -n "$1" ]; then
-  echo "Updating to $1" 
-  sudo systemctl stop mvnpm.service
-  ln -sf /home/pkruger/uploads/mvnpm-$1-runner mvnpm-runner
-  sudo systemctl start mvnpm.service
-else
-  echo "Please provide the version number as the first argument"
-fi
