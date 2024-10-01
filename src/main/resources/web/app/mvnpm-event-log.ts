692f22033c0c8f85a821d72d08510efc4d4d32fb
Andy Damevin <ia3andy@gmail.com>
2024-10-01 17:55:08 +0200

    Introduce live-sync checkbox

--- src/main/resources/web/app/mvnpm-event-log.ts
+++ src/main/resources/web/app/mvnpm-event-log.ts
@@ -1,114 +1,174 @@
-import { LitElement, html, css } from 'lit';
-import { customElement, state } from 'lit/decorators.js';
+import {LitElement, html, css} from 'lit';
+import {customElement, state, property} from 'lit/decorators.js';
+
+export const WEBSOCKET_BASE = window.location.protocol === "https:" ? "wss:" : "ws:" + "//" + window.location.host;
 
 /**
  * This component shows the Event log
  */
 @customElement('mvnpm-event-log')
 export class MvnpmEventLog extends LitElement {
-    static styles = css`
-    :host {
-      display: flex;
-      gap: 10px;
-      width: 100%;
-      max-height: 40vh;
-      background: black;
-    }
-    .console {
-      display: flex;
-      flex-direction: column;
-      width: 100%;
-      height: 100%;
-      padding-left: 20px;
-      padding-right: 20px;
-      background: black;
-      font-family: 'Courier New', monospace;
-      font-size: small;
-      filter: brightness(0.85);
-    }
-    .line {
-        display: flex;
-        flex-direction: row;
-        gap: 10px;
-    }
+  static styles = css`
+      :host {
+          display: flex;
+          gap: 10px;
+          width: 100%;
+          max-height: 40vh;
+          background: black;
+      }
+
+      .console {
+          display: flex;
+          flex-direction: column;
+          width: 100%;
+          height: 100%;
+          padding-left: 20px;
+          padding-right: 20px;
+          background: black;
+          font-family: 'Courier New', monospace;
+          font-size: small;
+          filter: brightness(0.85);
+      }
+
+      .line {
+          display: flex;
+          flex-direction: row;
+          gap: 10px;
+      }
 
   `;
 
-    @state({ type: Array })
-    private _initEventLog: any[] | null = null;
+  private _logWebSocket = null;
 
-    private _eventLogEntry = (event: CustomEvent) => this._receiveLogEntry(event.detail);
+  @state({type: Array})
+  private _initEventLog: any[] | null = null;
 
-    constructor() {
-        super();
+  @property({type: Boolean})
+  private liveSync;
+
+  private _eventLogEntry = (event: CustomEvent) => this._receiveLogEntry(event.detail);
+
+  constructor() {
+    super();
+
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    fetch("/api/eventlog/top")
+      .then(response => response.json())
+      .then(initEventLog => (this._initEventLog = this._addMultipleToLog(this._initEventLog, initEventLog)));
+
+    document.addEventListener('eventLogEntryEvent', this._eventLogEntry, false);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this._stopLiveSync();
+    document.removeEventListener('eventLogEntryEvent', this._eventLogEntry, false);
+  }
+
+  updated(changedProps) {
+    if (this.liveSync) {
+      this._startLiveSync();
+    } else {
+      this._stopLiveSync();
     }
+  }
 
-    connectedCallback() {
-        super.connectedCallback();
-
-        fetch("/api/eventlog/top")
-            .then(response => response.json())
-            .then(initEventLog => (this._initEventLog = this._addMultipleToLog(this._initEventLog, initEventLog)));
-        
-        document.addEventListener('eventLogEntryEvent', this._eventLogEntry, false);
+  private _startLiveSync() {
+    if (!this._logWebSocket) {
+      this._logWebSocket = new WebSocket(WEBSOCKET_BASE + "/api/stream/eventlog");
+      this._logWebSocket.onmessage = function (event) {
+        var eventLogEntry = JSON.parse(event.data);
+        const eventLogEntryEvent = new CustomEvent('eventLogEntryEvent', {detail: eventLogEntry});
+        document.dispatchEvent(eventLogEntryEvent);
+      }
+      this._logWebSocket.onclose = function (event) {
+        setTimeout(function () {
+          if (this.liveSync) {
+            this._startLiveSync();
+          }
+        }, 100);
+      };
     }
+  }
 
-    disconnectedCallback() {
-        super.disconnectedCallback();
-        document.removeEventListener('eventLogEntryEvent', this._eventLogEntry, false);
+  private _stopLiveSync() {
+    if (this._logWebSocket) {
+      this._logWebSocket.close();
+      this._logWebSocket = null;
     }
+  }
 
-    private _receiveLogEntry(initEventLog: any) {
-        this._initEventLog = this._addToLog(this._initEventLog, initEventLog);
-    }
 
-    render() {
-        return html`
+  private _receiveLogEntry(initEventLog: any) {
+    this._initEventLog = this._addToLog(this._initEventLog, initEventLog);
+  }
+
+  render() {
+    return html`
       <div class="console">
         ${this._renderInitEventLog()}
       </div>
     `;
-    }
+  }
 
-    private _renderInitEventLog() {
-        if (this._initEventLog && this._initEventLog.length > 0) {
-            return html`
-                ${this._initEventLog.map((entry) => {
-                    return html`${this._renderLine(entry)}`
-                })}
-              `;
-        } else {
-            return html`<p>Nothing in the event log</p>`;
-        }
+  private _renderInitEventLog() {
+    if (this._initEventLog && this._initEventLog.length > 0) {
+      return html`
+        <l-dot-stream
+            size="20"
+            speed="2.5"
+            color="#66a5b1"
+        ></l-dot-stream><br/>
+        ${this._initEventLog.map((entry) => {
+          return html`${this._renderLine(entry)}`
+        })}
+      `;
+    } else if (this.liveSync) {
+      return html`
+        <p>Nothing in the event log <br/>
+          <l-dot-stream
+              size="20"
+              speed="2.5"
+              color="#66a5b1"
+          ></l-dot-stream>
+        </p>`;
+    } else {
+      return html`<p>Nothing in the event log</p>`;
     }
+  }
 
-    private _renderLine(entry){
-        let formattedTime = entry.time.substring(0, entry.time.indexOf(".")).replace('T',' ');
-        
-        return html`<div class="line">
-                        <span style="color: grey">${formattedTime}</span>
-                        <span style="color: lightblue">${entry.groupId}</span>
-                        <span style="color: lightyellow">${entry.artifactId}</span>
-                        <span style="color: lightpink">${entry.version}</span>
-                        <span style="color: lightgrey">[${entry.stage}]</span>
-                        <span style="color: ${entry.color}">${entry.message}</span>
-                    </div>`;
-    }
+  private _renderLine(entry) {
+    let formattedTime = entry.time.substring(0, entry.time.indexOf(".")).replace('T', ' ');
 
-    private _addToLog(queue: any[] | null, item: any) {
-        if (queue && queue.length > 0) {
-            return [item, ...queue];
-        } else {
-            return [item];
-        }
-    }
+    return html`
+      <div class="line">
+        <span style="color: grey">${formattedTime}</span>
+        <span style="color: lightblue">${entry.groupId}</span>
+        <span style="color: lightyellow">${entry.artifactId}</span>
+        <span style="color: lightpink">${entry.version}</span>
+        <span style="color: lightgrey">[${entry.stage}]</span>
+        <span style="color: ${entry.color}">${entry.message}</span>
+      </div>`;
+  }
 
-    private _addMultipleToLog(queue: any[] | null, items: any[]) {
-        if (queue && queue.length > 0) {
-            return [...items, ...queue];
-        } else {
-            return items;
-        }
+  private _addToLog(queue: any[] | null, item: any) {
+    if (queue && queue.length > 0) {
+      return [item, ...queue];
+    } else {
+      return [item];
     }
+  }
+
+  private _addMultipleToLog(queue: any[] | null, items: any[]) {
+    if (queue && queue.length > 0) {
+      return [...items, ...queue];
+    } else {
+      return items;
+    }
+  }
 
 }
--- src/main/resources/web/app/mvnpm-progress.ts
+++ src/main/resources/web/app/mvnpm-progress.ts
@@ -1,8 +1,9 @@
-import { LitElement, html, css } from 'lit';
-import { customElement, state } from 'lit/decorators.js';
+import {LitElement, html, css} from 'lit';
+import {customElement, state, property} from 'lit/decorators.js';
 import '@vaadin/progress-bar';
 import '@vaadin/message-list';
-import { dotStream } from 'ldrs';
+import {dotStream} from 'ldrs';
+import {WEBSOCKET_BASE} from "./mvnpm-event-log";
 
 dotStream.register();
 
@@ -11,125 +12,174 @@ dotStream.register();
  */
 @customElement('mvnpm-progress')
 export class MvnpmProgress extends LitElement {
-    static styles = css`
-    :host {
-      display: flex;
-      gap: 10px;
-      width: 100%;
-    }
-    .lane {
-      display: flex;
-      gap: 10px;
-      flex-direction: column;
-      width: 100%;
-      height: 100%;
-      padding-left: 20px;
-      padding-right: 20px;
-      align-items: center;
-    }
-    .maven-central {
-        width: 100%;
-        display: flex;
-        gap: 10px;
-        flex-direction: column;
-        width: 100%;
-        height: 100%;
-        padding-left: 20px;
-        padding-right: 20px;
-        align-items: center;
-        overflow-y: scroll;
-    }
-    .maven-central-message-box {
-        width: 100%;
-        overflow-y: unset;
-    }
-    .uploading {
-      border-right: 2px solid var(--lumo-contrast-10pct);
-      border-left: 2px solid var(--lumo-contrast-10pct);
-    }
-    .progressBar {
-      width: 50%;
-    }
-    vaadin-message-list {
-        width: 100%;
-    }
+
+
+  static styles = css`
+      :host {
+          display: flex;
+          gap: 10px;
+          width: 100%;
+      }
+
+      .lane {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          width: 100%;
+          height: 100%;
+          padding-left: 20px;
+          padding-right: 20px;
+          align-items: center;
+      }
+
+      .maven-central {
+          width: 100%;
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          width: 100%;
+          height: 100%;
+          padding-left: 20px;
+          padding-right: 20px;
+          align-items: center;
+          overflow-y: scroll;
+      }
+
+      .maven-central-message-box {
+          width: 100%;
+          overflow-y: unset;
+      }
+
+      .uploading {
+          border-right: 2px solid var(--lumo-contrast-10pct);
+          border-left: 2px solid var(--lumo-contrast-10pct);
+      }
+
+      .progressBar {
+          width: 50%;
+      }
+
+      vaadin-message-list {
+          width: 100%;
+      }
   `;
 
-    @state({ type: Array })
-    private _initQueue: any[] | null = null;
+  private _syncWebSocket = null;
 
-    @state({ type: Array })
-    private _uploadingQueue: any[] | null = null;
+  @state({type: Array})
+  private _initQueue: any[] | null = null;
 
-    @state({ type: Array })
-    private _uploadedQueue: any[] | null = null;
+  @state({type: Array})
+  private _uploadingQueue: any[] | null = null;
 
-    @state({ type: Array })
-    private _closedQueue: any[] | null = null;
+  @state({type: Array})
+  private _uploadedQueue: any[] | null = null;
 
-    @state({ type: Array })
-    private _releasingQueue: any[] | null = null;
+  @state({type: Array})
+  private _closedQueue: any[] | null = null;
 
-    @state({ type: Array })
-    private _releasedQueue: any[] | null = null;
+  @state({type: Array})
+  private _releasingQueue: any[] | null = null;
 
-    private _centralSyncStateChange = (event: CustomEvent) => this._receiveStateChange(event.detail);
+  @state({type: Array})
+  private _releasedQueue: any[] | null = null;
 
-    constructor() {
-        super();
+  @property({type: Boolean})
+  private liveSync;
+
+  private _centralSyncStateChange = (event: CustomEvent) => this._receiveStateChange(event.detail);
+
+  constructor() {
+    super();
+  }
+
+  updated(changedProps) {
+    if (this.liveSync) {
+      this._startLiveSync();
+    } else {
+      this._stopLiveSync();
     }
+  }
 
-    connectedCallback() {
-        super.connectedCallback();
-
-        fetch("/api/sync/item/INIT")
-            .then(response => response.json())
-            .then(initQueue => (this._initQueue = this._addMultipleToQueue(this._initQueue, initQueue, "Waiting...", 1)));
-        fetch("/api/sync/item/UPLOADING")
-            .then(response => response.json())
-            .then(uploadingQueue => (this._uploadingQueue = this._addMultipleToQueue(this._uploadingQueue, uploadingQueue, "Uploading to maven central...", 2)));
-        fetch("/api/sync/item/UPLOADED")
-            .then(response => response.json())
-            .then(uploadedQueue => (this._uploadedQueue = this._addMultipleToQueue(this._uploadedQueue, uploadedQueue, "Uploaded, validating...", 3)));
-        fetch("/api/sync/item/CLOSED")
-            .then(response => response.json())
-            .then(closedQueue => (this._closedQueue = this._addMultipleToQueue(this._closedQueue, closedQueue, "Valid, preparing release...", 4)));
-        fetch("/api/sync/item/RELEASING")
-            .then(response => response.json())
-            .then(releasingQueue => (this._releasingQueue = this._addMultipleToQueue(this._releasingQueue, releasingQueue, "Releasing to maven central...", 5)));
-
-        document.addEventListener('centralSyncStateChange', this._centralSyncStateChange, false);
+  private _startLiveSync() {
+    if (!this._syncWebSocket) {
+      this._syncWebSocket = new WebSocket(WEBSOCKET_BASE + "/api/queue/");
+      this._syncWebSocket.onmessage = function (event) {
+        var centralSyncItem = JSON.parse(event.data);
+        const centralSyncStateChangeEvent = new CustomEvent('centralSyncStateChange', {detail: centralSyncItem});
+        document.dispatchEvent(centralSyncStateChangeEvent);
+      }
+      const that = this;
+      this._syncWebSocket.onclose = function (event) {
+        setTimeout(function () {
+          if (that.liveSync) {
+            that._syncWebSocket = null;
+            that._startLiveSync();
+          }
+        }, 100);
+      };
     }
+  }
 
-    disconnectedCallback() {
-        super.disconnectedCallback();
-        document.removeEventListener('centralSyncStateChange', this._centralSyncStateChange, false);
+  private _stopLiveSync() {
+    if (this._syncWebSocket) {
+      this._syncWebSocket.close();
+      this._syncWebSocket = null;
     }
+  }
 
-    private _receiveStateChange(centralSyncItem: any) {
-        if (centralSyncItem.stage === "INIT") {
-            this._uploadingQueue = this._removeFromQueue(this._uploadingQueue, centralSyncItem);
-            this._initQueue = this._addToQueue(this._initQueue, centralSyncItem, "Waiting...", 1);
-        } else if (centralSyncItem.stage === "UPLOADING") {
-            this._initQueue = this._removeFromQueue(this._initQueue, centralSyncItem);
-            this._uploadingQueue = this._addToQueue(this._uploadingQueue, centralSyncItem, "Uploading to maven central", 2);
-        } else if (centralSyncItem.stage === "UPLOADED") {
-            this._uploadingQueue = this._removeFromQueue(this._uploadingQueue, centralSyncItem);
-            this._uploadedQueue = this._addToQueue(this._uploadedQueue, centralSyncItem, "Uploaded, validating...", 3);
-        } else if (centralSyncItem.stage === "CLOSED") {
-            this._uploadedQueue = this._removeFromQueue(this._uploadedQueue, centralSyncItem);
-            this._closedQueue = this._addToQueue(this._closedQueue, centralSyncItem, "Valid, preparing release...", 4);
-        } else if (centralSyncItem.stage === "RELEASING") {
-            this._closedQueue = this._removeFromQueue(this._closedQueue, centralSyncItem);
-            this._releasingQueue = this._addToQueue(this._releasingQueue, centralSyncItem, "Releasing to maven central...", 5);
-        } else if (centralSyncItem.stage === "RELEASED") {
-            this._releasingQueue = this._removeFromQueue(this._releasingQueue, centralSyncItem);
-            this._releasedQueue = this._addToQueue(this._releasedQueue, centralSyncItem, "Released.", 6);
-        }
+  connectedCallback() {
+    super.connectedCallback();
+
+    fetch("/api/sync/item/INIT")
+      .then(response => response.json())
+      .then(initQueue => (this._initQueue = this._addMultipleToQueue(this._initQueue, initQueue, "Waiting...", 1)));
+    fetch("/api/sync/item/UPLOADING")
+      .then(response => response.json())
+      .then(uploadingQueue => (this._uploadingQueue = this._addMultipleToQueue(this._uploadingQueue, uploadingQueue, "Uploading to maven central...", 2)));
+    fetch("/api/sync/item/UPLOADED")
+      .then(response => response.json())
+      .then(uploadedQueue => (this._uploadedQueue = this._addMultipleToQueue(this._uploadedQueue, uploadedQueue, "Uploaded, validating...", 3)));
+    fetch("/api/sync/item/CLOSED")
+      .then(response => response.json())
+      .then(closedQueue => (this._closedQueue = this._addMultipleToQueue(this._closedQueue, closedQueue, "Valid, preparing release...", 4)));
+    fetch("/api/sync/item/RELEASING")
+      .then(response => response.json())
+      .then(releasingQueue => (this._releasingQueue = this._addMultipleToQueue(this._releasingQueue, releasingQueue, "Releasing to maven central...", 5)));
+
+    document.addEventListener('centralSyncStateChange', this._centralSyncStateChange, false);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this._stopLiveSync();
+    document.removeEventListener('centralSyncStateChange', this._centralSyncStateChange, false);
+  }
+
+  private _receiveStateChange(centralSyncItem: any) {
+    if (centralSyncItem.stage === "INIT") {
+      this._uploadingQueue = this._removeFromQueue(this._uploadingQueue, centralSyncItem);
+      this._initQueue = this._addToQueue(this._initQueue, centralSyncItem, "Waiting...", 1);
+    } else if (centralSyncItem.stage === "UPLOADING") {
+      this._initQueue = this._removeFromQueue(this._initQueue, centralSyncItem);
+      this._uploadingQueue = this._addToQueue(this._uploadingQueue, centralSyncItem, "Uploading to maven central", 2);
+    } else if (centralSyncItem.stage === "UPLOADED") {
+      this._uploadingQueue = this._removeFromQueue(this._uploadingQueue, centralSyncItem);
+      this._uploadedQueue = this._addToQueue(this._uploadedQueue, centralSyncItem, "Uploaded, validating...", 3);
+    } else if (centralSyncItem.stage === "CLOSED") {
+      this._uploadedQueue = this._removeFromQueue(this._uploadedQueue, centralSyncItem);
+      this._closedQueue = this._addToQueue(this._closedQueue, centralSyncItem, "Valid, preparing release...", 4);
+    } else if (centralSyncItem.stage === "RELEASING") {
+      this._closedQueue = this._removeFromQueue(this._closedQueue, centralSyncItem);
+      this._releasingQueue = this._addToQueue(this._releasingQueue, centralSyncItem, "Releasing to maven central...", 5);
+    } else if (centralSyncItem.stage === "RELEASED") {
+      this._releasingQueue = this._removeFromQueue(this._releasingQueue, centralSyncItem);
+      this._releasedQueue = this._addToQueue(this._releasedQueue, centralSyncItem, "Released.", 6);
     }
+  }
 
-    render() {
-        return html`
+  render() {
+    return html`
       <div class="lane">
         <h3>Queue</h3>
         ${this._renderInitQueue()}
@@ -142,95 +192,100 @@ export class MvnpmProgress extends LitElement {
         ${this._renderMavenCentral()}
       </div>
     `;
+  }
+
+  private _renderInitQueue() {
+    if (this._initQueue && this._initQueue.length > 0) {
+      return html`
+        <vaadin-message-list .items="${this._initQueue}"></vaadin-message-list>
+        <l-dot-stream
+            size="30"
+            speed="2.5"
+            color="#66a5b1"
+        ></l-dot-stream>`;
+    } else if (this.liveSync) {
+      return html`
+        <l-dot-stream
+            size="40"
+            speed="2.5"
+            color="#66a5b1"
+        ></l-dot-stream> <br/>Waiting for items
+      `;
+    } else {
+      return html`No items`;
+    }
+  }
+
+  private _renderUploading() {
+    if (this._uploadingQueue && this._uploadingQueue.length > 0) {
+      return html`
+        <vaadin-progress-bar class="progressBar" indeterminate></vaadin-progress-bar>
+        <vaadin-message-list .items="${this._uploadingQueue}"></vaadin-message-list>
+      `;
+    }
+  }
+
+  private _renderMavenCentral() {
+
+    let merged = [];
+    if (this._uploadedQueue && this._uploadedQueue.length > 0) {
+      merged = merged.concat(this._uploadedQueue);
+    }
+    if (this._closedQueue && this._closedQueue.length > 0) {
+      merged = merged.concat(this._closedQueue);
+    }
+    if (this._releasingQueue && this._releasingQueue.length > 0) {
+      merged = merged.concat(this._releasingQueue);
+    }
+    if (this._releasedQueue && this._releasedQueue.length > 0) {
+      merged = merged.concat(this._releasedQueue);
     }
 
-    private _renderInitQueue() {
-        if (this._initQueue && this._initQueue.length > 0) {
-            return html`<vaadin-message-list .items="${this._initQueue}"></vaadin-message-list>
-            <l-dot-stream
-                size="60"
-                speed="2.5"
-                color="#66a5b1" 
-            ></l-dot-stream>`;
-        } else {
-            return html`<l-dot-stream
-                size="60"
-                speed="2.5"
-                color="#66a5b1" 
-            ></l-dot-stream> <br/>Waiting for sync
-            `;
-        }
+    return html`
+      <div class="maven-central">
+        <vaadin-message-list class="maven-central-message-box" .items="${merged}"></vaadin-message-list>
+      </div>`;
+  }
+
+  private _addToQueue(queue: any[] | null, item: any, stagemessage: string, step: number) {
+    let mle = this._toMessageListEntry(item, stagemessage, step);
+    if (queue && queue.length > 0) {
+      return [...queue, mle];
+    } else {
+      return [mle];
+    }
+  }
+
+  private _addMultipleToQueue(queue: any[] | null, items: any[], stagemessage: string, step: number) {
+    let mles = items.map(item => this._toMessageListEntry(item, stagemessage, step));
+    if (queue && queue.length > 0) {
+      return [...queue, ...mles];
+    } else {
+      return mles;
+    }
+  }
+
+  private _removeFromQueue(queue: any[] | null, item: any) {
+    if (queue && queue.length > 0) {
+      var index = queue.findIndex(mle => {
+        return mle.time == item.groupId && mle.userName == item.artifactId + " " + item.version;
+      });
+      queue.splice(index, 1);
     }
 
-    private _renderUploading() {
-        if (this._uploadingQueue && this._uploadingQueue.length > 0) {
-            return html`
-                <vaadin-progress-bar class="progressBar" indeterminate></vaadin-progress-bar>
-                <vaadin-message-list .items="${this._uploadingQueue}"></vaadin-message-list>
-            `;
-        }
+    if (queue && queue.length == 0) {
+      return null;
+    } else {
+      return [...queue];
     }
+  }
 
-    private _renderMavenCentral() {
-        
-        let merged = [];
-        if (this._uploadedQueue && this._uploadedQueue.length > 0) {
-            merged = merged.concat(this._uploadedQueue);
-        }
-        if (this._closedQueue && this._closedQueue.length > 0) {
-            merged = merged.concat(this._closedQueue);
-        }
-        if (this._releasingQueue && this._releasingQueue.length > 0) {
-            merged = merged.concat(this._releasingQueue);
-        }
-        if (this._releasedQueue && this._releasedQueue.length > 0) {
-            merged = merged.concat(this._releasedQueue);
-        }
-        
-        return html`<div class="maven-central">
-                        <vaadin-message-list class="maven-central-message-box" .items="${merged}"></vaadin-message-list>
-                    </div>`;
-    }
-
-    private _addToQueue(queue: any[] | null, item: any, stagemessage: string, step: number) {
-        let mle = this._toMessageListEntry(item, stagemessage, step);
-        if (queue && queue.length > 0) {
-            return [...queue, mle];
-        } else {
-            return [mle];
-        }
-    }
-
-    private _addMultipleToQueue(queue: any[] | null, items: any[], stagemessage: string, step: number) {
-        let mles = items.map(item => this._toMessageListEntry(item, stagemessage, step));
-        if (queue && queue.length > 0) {
-            return [...queue, ...mles];
-        } else {
-            return mles;
-        }
-    }
-
-    private _removeFromQueue(queue: any[] | null, item: any) {
-        if (queue && queue.length > 0) {
-            var index = queue.findIndex(mle => {
-                return mle.time == item.groupId && mle.userName == item.artifactId + " " + item.version;
-            });
-            queue.splice(index, 1);
-        }
-
-        if (queue && queue.length == 0) {
-            return null;
-        } else {
-            return [...queue];
-        }
-    }
-
-    private _toMessageListEntry(item: any, stagemessage: string, step: number) {
-        return {
-            text: stagemessage + " (" + item.uploadAttempts + ")" ,
-            time: item.groupId,
-            userName: item.artifactId + " " + item.version,
-            userColorIndex: step
-        };
-    }
+  private _toMessageListEntry(item: any, stagemessage: string, step: number) {
+    return {
+      text: stagemessage + " (" + item.uploadAttempts + ")",
+      time: item.groupId,
+      userName: item.artifactId + " " + item.version,
+      userColorIndex: step
+    };
+  }
 }
