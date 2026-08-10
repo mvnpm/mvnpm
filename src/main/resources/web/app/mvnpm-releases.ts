05ac2ba6436ae026b65c9ea0f8a400066e64e3bb
Phillip Kruger <phillip.kruger@gmail.com>
2026-08-10 16:36:45 +1000

    Fix grid column binding from stagingRepoId to releaseId

--- src/main/resources/web/app/mvnpm-releases.ts
+++ src/main/resources/web/app/mvnpm-releases.ts
@@ -119,7 +119,7 @@ export class MvnpmReleases extends LitElement {
                 <vaadin-grid-sort-column header="Artifact Id" path="artifactId"></vaadin-grid-sort-column>
                 <vaadin-grid-sort-column header="Version" path="version"></vaadin-grid-sort-column>
                 <vaadin-grid-sort-column header="Last modified" path="stageChangeTime"></vaadin-grid-sort-column>
-                <vaadin-grid-sort-column header="Release Id" path="stagingRepoId"></vaadin-grid-sort-column>
+                <vaadin-grid-sort-column header="Release Id" path="releaseId"></vaadin-grid-sort-column>
                 <vaadin-grid-sort-column header="Upload attempts" path="uploadAttempts"></vaadin-grid-sort-column>
                 ${this._renderActionCol()}
                 
